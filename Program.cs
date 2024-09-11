a6b26f63b51fadb281a81f1d17b712c0eefef01a
SaraLemus01 <sara.lemus.2004.10@gmail.com>
2024-09-11 00:01:00 -0600

    Modificacion del archivo program

--- Program.cs
+++ Program.cs
@@ -16,29 +16,69 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+//Crear una lista para almacenar objectos 
+var categorys = new List<Category>();
 
-app.MapGet("/weatherforecast", () =>
+//Configura una ruta GET 
+app.MapGet("/categorys", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+return categorys; //devuelve la lista 
+});
 
+// Configura una ruta GET 
+app.MapGet("/categorys/{id}", (int id) =>
+{
+// Busca en la lista que tenga su ID especificado
+var category = categorys.FirstOrDefault(c => c.Id == id);
+return category;
+});
+//Configura una ruta Post 
+app.MapPost("/categorys", (Category category) =>
+{
+categorys.Add(category);
+return Results.Ok();//Devuelve una respuesta HTTP 200 OK
+});
+
+//Configura una rut PUT  para actualizar 
+app.MapPut("/categorys/{id}", (int id, Category category) =>
+{
+
+var existingCategory = categorys.FirstOrDefault(c => c.Id == id);
+if (existingCategory != null)
+{
+//Actualiza los datos
+existingCategory.Name = category.Name;
+existingCategory.description = category.description;
+return Results.Ok();//Devuelve una respuesta HTTP 200 ok
+}
+else
+{
+return Results.NotFound();//Devuelve una respuesta HTTP 404 Not Fount 
+}
+});
+
+//Configra una ruta DELETE par eliminar 
+app.MapDelete("/categorys/{id}", (int id) =>
+{
+var existingCategory = categorys.FirstOrDefault(c => c.Id == id);
+if (existingCategory != null)
+{
+//Elimina
+categorys.Remove(existingCategory);
+return Results.Ok();//Deuelve una respuesta HTTP 200 Ok
+}
+else
+{
+return Results.NotFound();//Devuelve una respuesta HTTP 404
+}
+});
+//Ejecutar la aplicacion
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+//Definicion de la clase
+internal class Category
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string description { get; set; }
 }
--- obj/Debug/net8.0/SYLM10092024.MvcApplicationPartsAssemblyInfo.cs
+++ obj/Debug/net8.0/SYLM10092024.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SYLM10092024")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad91e83c289d9c5fbbf9bd399ca6a937dfc0ebc")]
-[assembly: System.Reflection.AssemblyProductAttribute("SYLM10092024")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SYLM10092024")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.OpenApi")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
